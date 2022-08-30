f9981250657031da46a3a5080f0a1a886d5e3d6e
wajiw <wcasey@vailsys.com>
2022-08-30 13:49:48 -0500

    update

--- SenecaMadLibs.java
+++ SenecaMadLibs.java
@@ -4,7 +4,8 @@ public class SenecaMadLibs {
     public static void main(String[] args) {
         Console console = System.console();
         // printf to be able to use format specifiers
+        // format specifier for string %s
+        console.printf("this class is awesome\n\n");
 
-        
     }
 }
