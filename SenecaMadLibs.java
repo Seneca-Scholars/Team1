074c181b9b8065c3d7b4badb1403f5363d606e82
JAYYZWORLD <jalank15@gmail.com>
2022-08-30 15:22:11 -0500

    It's the homework

--- SenecaMadLibs.java
+++ SenecaMadLibs.java
@@ -5,7 +5,13 @@ public class SenecaMadLibs {
         Console console = System.console();
         // printf to be able to use format specifiers
         // format specifier for string %s
-        console.printf("this class is awesome\n\n");
+        console.printf("this class is cool\n\n");
+        // printf to be able to use format specifiers
+        // format specifier for string %s
+        String name = console.readLine("Enter your name:  ");
+    
 
+        console.printf("is amazing\n", name);
+        
     }
 }
