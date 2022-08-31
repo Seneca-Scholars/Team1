91df85dd01d246fcee5b51d8c870c942c2e4087c
aflores1017 <aflores@senecafoundation.org>
2022-08-31 15:26:37 -0500

    Homework

--- SenecaMadLibs.java
+++ SenecaMadLibs.java
@@ -1,12 +1,21 @@
 import java.io.Console;
 
 public class SenecaMadLibs {
+
+
     public static void main(String[] args) {
         Console console = System.console();
         // printf to be able to use format specifiers
         // format specifier for string %s
+        console.printf("this class is awesome\n" );
+
         String name = console.readLine("Enter Your Name: ");
-        console.printf("%s is an awesome student\n\n", name);
+        String food = console.readLine("Enter Type Of Food: ");
+        String drink = console.readLine("Enter Type Of Drink: ");
+        console.printf("%s wants to eat a %s and wants to drink a %s\n\n", name ,food,drink);
+        if (food.equals("mango")) {
+            console.printf("mangos are ehh");
+        }
 
     }
 }
