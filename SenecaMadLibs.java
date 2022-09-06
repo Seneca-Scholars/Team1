053ca06e7cb485f3412e6a4921a5cfadc70f0a6b
wajiw <wcasey@vailsys.com>
2022-09-06 14:03:46 -0500

    mad libs updates

--- SenecaMadLibs.java
+++ SenecaMadLibs.java
@@ -4,9 +4,23 @@ public class SenecaMadLibs {
     public static void main(String[] args) {
         Console console = System.console();
         // printf to be able to use format sp
-        String name = console.readLine("Enter your name:  ");
+        String age = console.readLine("Enter your age:  ");
+        int newAge = Integer.parseInt(age);
 
-        console.printf("%s is an awesome student!\n", name);
-        
+        if(newAge < 14) {
+            console.printf("You're too young to use this program. Goodbye!\n");
+            System.exit(0);
+        }
+
+        String name = console.readLine("Enter your name:  ");
+        String adjective = console.readLine("Enter a adjective: ");
+        String adverb = console.readLine("Enter a adverb:  ");
+        String verb = console.readLine("Enter a verb: ");
+        String noun = console.readLine("Enter a noun: ");
+
+        console.printf("Your story:\n-------------\n");
+        console.printf("%s is a %s %s\n", name, adjective, noun);
+        console.printf("They are always %s %s\n", verb, adverb); 
+
     }
 }
