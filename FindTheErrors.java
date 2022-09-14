66e3dccf55fc40ea9c6a5a903d753abd673ab51d
wajiw <31252+wajiw@users.noreply.github.com>
2022-09-14 16:18:57 -0500

    Revert "[Rapheal] SS-Homework Pull Requests"

--- FindTheErrors.java
+++ FindTheErrors.java
@@ -2,20 +2,20 @@ import java.io.Console;
 
 public class FindTheErrors {
     public static void main(String[] args) {
-        Console console = System.console();
+        //Console console = System.Console();
 
-        String firstName = console.readLine("Enter your first name:  ");
-        String lastName = console.readLine("Enter your last name:  ");
-        String ageString = console.readLine("Enter your age:  ");
+        //string firstName = console.readLine("Enter your first name:  ");
+        //String lastName = console.readLine("Enter your last name:  ")
+        //String ageString = console.readline("Enter your age:  ");
 
-        int age = Integer.parseInt(ageString);
+        //int age = Integer.parseInt(ageString);
 
-         if(age < 14) {
-             console.printf("Sorry, but %s is too young to use this app. \n", age);
-             System.exit(0);
-         }
+        // if(age < 14) {
+        //     console.printf("Sorry, but %s is too young to use this app. \n", age);
+        //     System.exit("0");
+        // }
 
-         console.printf("Welcome to our app %s %s!", firstName, lastName);
+        // console.print("Welcome to our app %s %s %s!", firstName, lastName);
     }
     
 }
