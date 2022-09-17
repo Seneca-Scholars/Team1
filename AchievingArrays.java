e5a2f87b863259c97d6d20af279030a0fea114da
wajiw <wcasey@vailsys.com>
2022-09-16 20:51:06 -0500

    Homework for next week

---
+++ AchievingArrays.java
@@ -0,0 +1,45 @@
+import java.io.Console;
+
+public class AchievingArrays {
+  public static void main(String[] args) {
+  Console console = System.console();
+
+  /*
+    *  ARRAYS PRACTICE!
+    * 
+    * Uncomment each of the if statements and fix the code so it 
+    * outputs for each statement based on the print
+    * 
+    */
+
+    String[] teachers = new String[3];
+    teachers[0] = "Jay";
+    teachers[1] = "Dave";
+    teachers[2] = "James";
+
+    // TODO: Replace null with an array literal that matches the long form above
+    String[] teachersLongForm = null;
+
+    String[] programmers = {
+            "Yukihiro Matsumoto",
+            "David Nolen",
+            "Grace Hopper",
+            "Linus Torvalds",
+            "You"
+    };
+
+  System.out.println("Choose a programmer:");
+  // TODO: Print out a menu by looping through the programmers array.
+  /*
+    The menu should be in the form of (each on a line of its own, starting with 1):
+    1. Yukihiro Matsumoto
+    2. David Nolen
+    ...
+  */
+  
+
+
+  }
+
+    
+}
--- ConfiguringConditionals.java
+++ ConfiguringConditionals.java
@@ -1,8 +1,44 @@
 import java.io.Console;
 
 public class ConfiguringConditionals {
-    Console console = System.console();
+  public static void main(String[] args) {
+  Console console = System.console();
 
-    
+  /*
+    *  CONDITIONALS PRACTICE!
+    * 
+    * Uncomment each of the if statements and fix the code so it 
+    * outputs for each statement based on the print
+    * 
+    */
+
+
+
+    int x = 3;
+    int y = 6;
+    int z = 12;
+    int a = 12;
+    int b = 36;
+    int c = 65;
+
+    // if () {
+    //   System.out.println("y is greater than x");
+    // }
+
+    // if () {
+    //   System.out.println("z is less than or equal to a");
+    // }
+
+    // if () {
+    //   System.out.println("b is less than y and c is greater than x");
+    // }
+
+    // if () {
+    //   System.out.println("z is greater than x + y and b is less than a + c");
+    // }
+
+
+  }
+
     
 }
