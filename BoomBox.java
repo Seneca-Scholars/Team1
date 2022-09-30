093f41281e46ad06c2119b659b5aa64d6a4aa2dc
wajiw <wcasey@vailsys.com>
2022-09-30 17:06:01 -0500

    Adding homework for Sept 30-Oct 7

---
+++ BoomBox.java
@@ -0,0 +1,21 @@
+// MAKE A BOOMBOX!
+//
+// Steps:
+//  1. Create a new object (in this file) called BoomBox
+//  2. Add a property for the `name` of the Boom Box
+//  3. Add a constructor that has a String argument that will 
+//    be stored in the `name` field
+//  4. Declare a variable that will store if it's on or off. 
+//    It should be off by default
+//  5. After you store the name output to the user: 
+//    "{NAME} the BoomBox has been created!"
+//  6. Add a method to turn on the BoomBox
+//  7. Create a new string array called `songs`
+//  8. Create a method called `addSongs` that adds strings to 
+//    our songs property (these should represent real songs)
+//  9. Create a method called `playSongs()` that outputs the song
+//    names to the user
+// 10. Create a method called `turnOff`
+// 11. Update `playSongs` so if the BoomBox is turned off instead
+//     of outputting to the user the songs it instead says:
+//     "{NAME} is turned off and cannot play any songs"
---
+++ PlaySomeTunes.java
@@ -0,0 +1,27 @@
+import java.io.Console;
+
+public class FindTheErrors {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to my BoomBox Player!");
+
+        // Steps 1-4:
+        // BoomBox myBoomBox = new BoomBox("Seneca Sweet Songs");
+
+        // Steps 5-6:
+        // myBoomBox.turnOn();
+
+        // Steps 7-8:
+        // myBoomBox.addSongs();
+
+        // Step 9:
+        // myBoomBox.playSongs();
+
+        // Step 10:
+        // myBoomBox.turnOff();
+
+        // Step 11:
+        // myBoomBox.playSongs();
+
+    }
+}
