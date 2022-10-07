31af5abbd1dd84dd2c972101508e0403d186def7
wajiw <wcasey@vailsys.com>
2022-10-07 10:29:11 -0500

    Map/List homework

---
+++ BoomBox/Song.java
@@ -0,0 +1,21 @@
+// MAKE A BOOMBOX!
+//
+// Steps:
+// Create the Song class:
+//  1. Create a new object (in this file) called Song
+//  2. Add a property for the `name` of the Song
+//  3. Add another property for the genre of the Song
+//  4. Add a property to store the length of the song
+//  5. Add a constructor with arguments for each of those properties
+//  6. Initialize the properties with the constructor arguments
+
+//  Update the BoomBox:
+//  1. Change the `songs` property to be a List with the new Song class as the Datatype
+//  2. Update `addSongs` to replace the String objects with Song objects (be creative por favor)
+//  3. Update `playSongs()` to use the new songs List
+
+// Update the Application:
+// 1. Add a Map called `players` so we can have multiple BoomBox objects at once
+// 2. Create 3 new BoomBox Objects
+// 3. Add the BoomBox Objects to the `players` map using different genres as keys
+// 4. Loop through `players` and call `playSongs()` on each instance in the loop
